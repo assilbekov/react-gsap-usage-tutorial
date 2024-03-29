d5fd847038f489f78223d66b2587f24dbac9e77c
assilbekov <askhat.assilbekov@gmail.com>
2024-03-29 04:29:35 -0300

    init content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="relative">
+      <div className="h-screen p-10 bg-gray-50 absolute top-0 left-0 z-10 w-full flex flex-col gap-10 tracking-tight">
+        <h1>Software Engineer</h1>
+        <h1>Designer</h1>
+        <h1>Freelancer</h1>
+      </div>
+      <div className="h-screen flex bg-gray-950 justify-center place-items-center">
+        <h1 className="text-9xl font-bold text-gray-100">
+          Welcome.
+        </h1>
+      </div>
+    </div>
   )
 }
 
