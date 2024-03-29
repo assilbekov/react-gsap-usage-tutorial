29ab45a4400a594f654c8e7db41187c068a8735b
assilbekov <askhat.assilbekov@gmail.com>
2024-03-29 04:52:18 -0300

    proj finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
+import gsap from "gsap";
+import { useLayoutEffect, useRef } from "react"
+
 function App() {
+  const rootRef = useRef(null);
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const t1 = gsap.timeline()
+      t1.from("#intro-slider", {
+        xPercent: "-100",
+        duration: 1.3,
+        delay: 0.3,
+      })
+        .from(["#title-1", "#title-2", "#title-3"], {
+          opacity: 0,
+          y: "+=30",
+          stagger: 0.5,
+        })
+        .to(["#title-1", "#title-2", "#title-3"], {
+          opacity: 0,
+          y: "-=30",
+          delay: 0.3,
+          stagger: 0.5,
+        })
+        .to("#intro-slider", {
+          xPercent: "-100",
+          duration: 1.3,
+        })
+        .from("#welcome", {
+          opacity: 0,
+          duration: 0.5,
+        })
+    }, rootRef)
+
+    return () => ctx.revert();
+  }, [])
+
   return (
-    <div className="relative">
-      <div className="h-screen p-10 bg-gray-50 absolute top-0 left-0 z-10 w-full flex flex-col gap-10 tracking-tight">
-        <h1>Software Engineer</h1>
-        <h1>Designer</h1>
-        <h1>Freelancer</h1>
+    <div className="relative" ref={rootRef}>
+      <div
+        id="intro-slider"
+        className="h-screen p-10 bg-gray-50 absolute top-0 left-0 z-10 w-full flex flex-col gap-10 tracking-tight"
+      >
+        <h1 className="text-9xl" id="title-1">Software Engineer</h1>
+        <h1 className="text-9xl" id="title-2">Designer</h1>
+        <h1 className="text-9xl" id="title-3">Freelancer</h1>
       </div>
-      <div className="h-screen flex bg-gray-950 justify-center place-items-center">
+      <div id="welcome" className="h-screen flex bg-gray-950 justify-center place-items-center">
         <h1 className="text-9xl font-bold text-gray-100">
-          Welcome.
+          Welcome
         </h1>
       </div>
     </div>
